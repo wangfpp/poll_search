4032171c077fac372bc0defcadd030df68200998
wangfpp <wangfpp@gmail.com>
2020-03-29 10:54:43 +0800

    add: 寻找分类入口

--- server.py
+++ server.py
@@ -14,17 +14,31 @@ class Search:
         self.baseUrl = 'https://www.99zuowen.com'
 
     def searchFn(self):
+        '''寻找主入口的下级标签  并进行分类  返回标签的title href'''
         session = HTMLSession()
         r  = session.get(self.baseUrl, timeout = 10)
-        navBar = r.html.find('div.spec.clearfix>dl>dd>a[href]')
+        navBar = r.html.find('div.mainnav>ul.nav.clearfix>li')
         url_list = []
         length = len(navBar)
         for index in range(1, length):
+            catlogDict = {};
             element = navBar[index]
-            absolute_link = list(element.absolute_links)[0]
-            url_list.append(absolute_link)
-        return url_list
+            a_node = element.find('a[href]', first=True)
+            main_title, main_href = self.fetchTitlandHref(a_node)
 
+            sub_node = element.find('div.sub_nav>a', first=False)
+            catlogDict["sub"] = []
+            sub_node_length = len(sub_node)
+            for _index in range(0,sub_node_length):
+                title, href = self.fetchTitlandHref(sub_node[_index])
+                sub_catlog_dict = {"title": title, "href": href}
+                catlogDict["sub"].append(sub_catlog_dict)
+            catlogDict["main"] = {"title": main_title, "href": main_href}
+            url_list.append(catlogDict)
+        return url_list
+    def fetchTitlandHref(self,element):
+        title, href = element.text, list(element.absolute_links)[0]
+        return title, href
     def searchTxt(self, url, index):
         print('task {}     start'.format(index))
         time.sleep(random.random())
